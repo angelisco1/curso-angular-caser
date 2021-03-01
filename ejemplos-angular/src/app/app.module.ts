1b34b00c6e2437b705134d74f4c33315711b0e80
AngelVil92 <angelvil92@gmail.com>
2021-03-01 12:07:50 +0100

    Dia 1: proyecto angular

---
+++ ejemplos-angular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiPrimerComponenteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
