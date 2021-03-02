1c33f28b8dadb558209dce5906578e8077898cb6
AngelVil92 <angelvil92@gmail.com>
2021-03-02 10:54:46 +0100

    Día 2: input y output

--- ejemplos-angular/src/app/cmp-input-output/cmp-input-output.component.ts
+++ ejemplos-angular/src/app/cmp-input-output/cmp-input-output.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Sugus {
+  sabor: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-cmp-input-output',
   templateUrl: './cmp-input-output.component.html',
@@ -7,7 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CmpInputOutputComponent implements OnInit {
 
+  sugus: Array<Sugus> = [
+    { sabor: 'piña', color: 'blue' },
+    { sabor: 'naranja', color: 'orange' },
+  ]
+
   constructor() { }
+
+  cambiarSaborSugus(event) {
+    console.log('Se está cambiando el sabor...')
+    this.sugus[0].sabor = event;
+  }
 
   ngOnInit(): void {
   }
--- ejemplos-angular/src/app/cmp-input-output/sugus/sugus.component.ts
+++ ejemplos-angular/src/app/cmp-input-output/sugus/sugus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sugus',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SugusComponent implements OnInit {
 
+  @Input() sabor: string = '';
+  @Input() color: string = '';
+  @Output() saborCambiado = new EventEmitter<string>();
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeSugus(event) {
+    // console.log(event);
+    this.saborCambiado.emit(event.target.value);
+  }
+
+  getStyles() {
+    return {
+      width: '100px',
+      height: '100px',
+      backgroundColor: this.color
+    }
   }
 
 }
